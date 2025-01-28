075601bafa6a6f24aca06f9ef20448e26feeef65
FilipSzkarlat <filip.szkarlat@gmail.com>
2025-01-28 23:17:15 +0100

    3 extra articles are showing in basic home view on the screen 768px or wider, full-hero desktop view is ready

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const ViewMoreBtn = document.querySelector(".btn-view-more");
+const hiddenArticles = document.querySelectorAll(".hidden-article");
 
 // After click the button, the hero section will be hidden and the full-hero section will be displayed
 ViewMoreBtn.addEventListener("click", () => {
   document.querySelector(".hero").style.display = "none";
   document.querySelector(".full-hero").style.display = "flex";
   ViewMoreBtn.style.display = "none";
+  hiddenArticles.forEach((article) => {
+    article.style.display = "none";
+  });
   scrollToTop(); // Scroll to the top of the page
 });
 
